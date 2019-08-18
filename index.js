a83bb2318c5ba4e16c9bbb5c7deddf949b952eff
Cory Sanin <corysanin@outlook.com>
2019-08-18 18:52:10 -0500

    added optional config file

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 const Express = require('express');
 const request = require('request');
-const port = 8080;
-const useragent = 'p2z'
+const fs = require('fs');
+const useragent = 'p2z';
+const cfgfile = './config/config.json';
 
+let config = {
+    port: 8080,
+    blacklist: true,
+    domainlist: []
+}
 
-var http = new Express();
+/**
+ * Check if domain is on the blacklist
+ * @param {string} domain 
+ */
+function notBlacklisted(domain){
+    for(let i = 0; i < config.domainlist.length; i++){
+        if(domain.includes(config.domainlist[i])){
+            return !config.blacklist;
+        }
+    }
+    return config.blacklist;
+}
+
+let http = new Express();
 
 http.use('/', Express.static('http-root'));
 
 http.get('/out.xml', function (req, res) {
     let url = req.query.in;
-    if(!/^[a-z]/.test(url.toLowerCase())){
+    if(!/^[a-z]+:\/\//.test(url.toLowerCase())){
         url = 'http://' + url;
     }
-    if(!'user-agent' in req.headers || req.headers['user-agent'] !== useragent){
-        res.setHeader('Content-Type', 'text/xml;charset=UTF-8');
+    let domain = url.split('/');
+    domain = domain[2];
+    if(!'user-agent' in req.headers || req.headers['user-agent'] !== useragent && notBlacklisted(domain)){
         request({
             url,
             timeout: 2000,
@@ -23,11 +43,14 @@ http.get('/out.xml', function (req, res) {
                 'User-Agent': useragent
             }
         }, function (error, response, body) {
+            if(response && response.statusCode){
+                res.status(response.statusCode);
+            }
             if(error){
                 console.error('error:', error);
             }
-            if(response && response.statusCode){
-                res.status(response.statusCode);
+            else{
+                res.setHeader('Content-Type', 'text/xml;charset=UTF-8');
             }
             res.send(body);
         });
@@ -38,4 +61,21 @@ http.get('/out.xml', function (req, res) {
     }
 });
 
-http.listen(port, () => console.log(`patreon-to-zune listening on port ${port}!`));
+
+fs.readFile(cfgfile, 'utf8', function(err, data){
+    if(!err){
+        try{
+            data = JSON.parse(data);
+            for(let key in data){
+                config[key] = data[key];
+            }
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+    else{
+        console.log('No config file found. Loading default configuration.');
+    }
+    http.listen(config.port, () => console.log(`patreon-to-zune listening on port ${config.port}!`));
+})
